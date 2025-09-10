0f1f6f000f2c15a8541f4f7e46d34330f4b0f541
pawel uhma <paweluhma136@gmail.com>
2025-09-10 09:35:55 +0200

    public uri fix

--- rag_web/next.config.ts
+++ rag_web/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: 'https://mm8yf29n32.us-east-1.awsapprunner.com' || 'http://localhost:8000',
   },
 };
 
